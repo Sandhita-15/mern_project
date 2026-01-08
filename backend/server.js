495f8da80f10d7bf1f0baffd9e397bf8f6196507
sandhita saha <sandhitasaha15@gmail.com>
2026-01-08 17:03:03 +0530

    backend update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 4000;
+
 const HOST = process.env.HOST || "localhost";
 
 app.listen(PORT, HOST,() => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} ${HOST}...`);
 });
