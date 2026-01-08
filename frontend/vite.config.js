0b9ff072b96e0ed998b3f5223f2fa6c65a54cf7a
sandhita saha <sandhitasaha15@gmail.com>
2026-01-08 17:12:19 +0530

    frontend update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     host: true,  // or '0.0.0.0' - exposes to all network interfaces
+    allowedHosts: ["mern-project-frontend-3735.onrender.com"]
   }
 })
